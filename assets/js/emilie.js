33324a0f171631d3b2136783a1c0f2ff72e10c2f
emiliecathrine <119924714+emiliecathrine@users.noreply.github.com>
2023-03-27 13:16:09 +0200

    opgave 4 favicon + fejl rettet i opgave 1

--- assets/js/emilie.js
+++ assets/js/emilie.js
@@ -1,8 +1,22 @@
+
 /*1. Anvende javascript til at skifte farven i de sociale elementer til en mere passende farve (jeres vurdering) */
 const socialIcons = document.querySelectorAll(".fa");
 console.log(socialIcons)
 
-for(let i = 0; i<= socialIcons.length; i++){
+for(let i = 0; i < socialIcons.length; i++){
     socialIcons[i].style.color = "blue";
-}
+};
 
+/*4. Brug javascript til at indsætte et favicon - f.eks. det her http://hearthstoneapi.com/favicon.ico (I know...hvorfor ville man nogensinde gøre det irl...men for øvelsens skyld) */
+
+const headEl = document.querySelector("head");
+
+console.log(headEl);
+
+const newLink = document.createElement("link");
+
+newLink.setAttribute("rel", "icon");
+newLink.setAttribute("type", "image/x-icon");
+newLink.setAttribute("href", "http://hearthstoneapi.com/favicon.ico");
+
+headEl.append(newLink);
